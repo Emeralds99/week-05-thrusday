0c99595dd4f4d19fc4ae88e97c9a0d9b7e80d0bb
Peter Harrington <pharrington375@gmail.com>
2020-07-02 21:00:45 -0400

    Implement functions to answer practice problems

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,13 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
+let arr = [1, 2, 3, 4, 5, 6, 7]
+let n = 3
 
-	// If the loop ends, return true
+function dropRight(arr, n) {
+	// if n is larger than array length return []
+	if (n > arr.length) {
+		return []
+	}
+	// Use n to count in from the end to get the index
+	return arr.slice(0, (arr.length - n))
+	// return a slice of the array using the index as the end
 }
+console.log(dropRight(arr, n))
--- flatten.js
+++ flatten.js
@@ -1,9 +1,21 @@
+let arr = [1, 2, [3, 4], [5, 6], 7]
+
 function flatten(arr) {
 	// Create a new array
 	let newList = []
 	// Loop through every item in the array
-	// If the item is an array, add its items to the new array
-	// If not add the item itself to the new array
-
+	for (i = 0; i < arr.length; i++) {
+		// If the item is an array, add its items to the new array
+		if (Array.isArray(arr[i])) {
+			newList = newList.concat(arr[i])
+		}
+		// If not add the item itself to the new array
+		else {
+			newList.push(arr[i])
+		}
+	}
 	// return the new array
+	return newList
 }
+
+console.log(flatten(arr))
